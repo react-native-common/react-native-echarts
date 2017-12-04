f978d202636ead81a3192bdb0f1f2a406a5a65a4
whr <357114638@qq.com>
2017-12-04 16:27:13 +0800

    添加平台判断

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet } from 'react-native';
+import { WebView, View, StyleSheet,Platform} from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 const source = (Platform.OS === 'ios')||__DEV__? require('./tpl.html') : {'uri':'file:///android_asset/echarts/tpl.html'}
